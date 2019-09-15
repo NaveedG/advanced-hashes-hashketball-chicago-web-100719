bb9669f0cd6ebe1653d623280ffcdf3b7d8e6fd3
Naveed Ganjani <nganjani@protonmail.com>
2019-09-15 21:18:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,6 +181,7 @@ end
 
 def big_shoe_rebounds
   shoe_sizes = []
+  big_foot = nil
   game_hash.each do |location, team_data|
     team_data[:players].each do |player_hash|
       player_hash.each do |player_name, player_data|
